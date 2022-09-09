74110f38dabf73a16ef7f7e1f719f7e85770280a
Mihan <87608466+Computer4062@users.noreply.github.com>
2022-09-09 11:17:15 +0400

    windows_run_app
    windows_run_app

---
+++ run/webbrowser.py
@@ -0,0 +1,32 @@
+import webbrowser
+from tkinter import*
+from PIL import*
+from PIL import ImageTk, Image
+
+win = Tk()
+win.iconbitmap("icon2.ico")
+win.configure(bg='white')
+win.title("run")
+
+def buttonFunction():
+    url = textbox.get()
+    webbrowser.open(url)
+
+img = ImageTk.PhotoImage(Image.open("icon2.png"))
+
+l4 = Label(text="Enter a url or a application name to open \n and this programme would open it up for you")
+l4.configure(bg="white")
+l4.grid(row=0,column=2)
+
+l3 = Label(text="Open:")
+l3.configure(bg="white")
+l3.grid(row=1,column=0)
+
+l2 = Label(image = img,width=50,height=50)
+l2.configure(bg='white')
+l2.grid(row=0,column=0)
+
+textbox = Entry(width=50)
+textbox.grid(row=1,column=2)
+btn1 = Button(text="Enter",command=buttonFunction)
+btn1.grid(row=1,column=3)
