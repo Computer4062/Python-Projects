96288129145bae8aa4bda3edce01cb9cc7bfee8f
unknown <mihan.edirisinghe@gmail.com>
2023-04-28 14:50:57 +0400

    Chatbot Open AI
    Uses Open AI to create a chatbot. Uses Open AI's ChatGPT 3.5 turbo to respond to messages given by a user.

---
+++ OpenAI Chatbot/bot.py
@@ -0,0 +1,25 @@
+import openai
+
+API_KEY = open('API KEY', 'r').read()
+openai.api_key = API_KEY
+
+log = []
+
+while True:
+    message = input(">>")
+    if message.lower() == "exit":
+        break
+    else:
+        log.append({"role": "user", "content":message})
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = log
+        )
+
+        response = response['choices'][0]['message']['content']
+
+        print("ChatGPT: ")
+        for sentence in response.split('.'):
+            print(sentence)
+
+        log.append({"role":"assistant", "content":response})
