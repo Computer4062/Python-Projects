169423cc50ac3de2598566ffe3efbeb43da34f8a
Mihan <87608466+Computer4062@users.noreply.github.com>
2023-05-05 23:26:10 +0400

    Update text_to_speech.py

--- Text To Speech/text_to_speech.py
+++ Text To Speech/text_to_speech.py
@@ -1,17 +1,22 @@
 from win32com.client import Dispatch
 from tkinter import Entry, Tk, Button
 
+def speak(text):
+    speak = Dispatch("SAPI.SpVoice")
+    voices = speak.GetVoices()
+    for voice in voices:
+        if "Microsoft Zira Desktop" in voice.GetDescription():
+            speak.Voice = voice
+            break
+    speak.Speak(text)
+
 root = Tk()
 root.title("Speaker")
 
 textbox = Entry(relief = "solid", width=30)
-textbox.grid(row = 0,column = 0)
+textbox.grid(row = 0, column = 0)
 
-def speak (text):
-	speak = Dispatch("SAPI.Spvoice")
-	speak.Speak(text)
-
-b1 = Button (text="Speak",command = lambda:speak(textbox.get()))
-b1.grid(row = 0,column = 1)
+b1 = Button (text="Speak",command = lambda:speak(textbox.get()), justify = 'center')
+b1.grid(row = 0, column = 1)
 
 root.mainloop()
