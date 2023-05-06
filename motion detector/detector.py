f344eb8746a10925464ed0d3a3334f4aa77ff0d5
Mihan <87608466+Computer4062@users.noreply.github.com>
2023-05-06 13:14:15 +0400

    Update detector.py

--- motion detector/detector.py
+++ motion detector/detector.py
@@ -1,14 +1,13 @@
 import cv2
-import imutils
 import threading
 import winsound
+import time
 
 camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 _, start_frame = camera.read()
-state_frame = imutils.resize(start_frame, width = 500)
 start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
 start_frame = cv2.GaussianBlur(start_frame, (21, 21), 8)
 
@@ -20,33 +19,30 @@ def beep():
 
 while True:
     _, frame = camera.read()
-    frame = imutils.resize(frame, width = 500)
 
-    if alarm_mode:
+    if alarm_mode == True:
         frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_bw = cv2.GaussianBlue(frame_bw, (5, 5), 0)
+        frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
 
         difference = cv2.absdiff(frame_bw, start_frame)
-        threshold = cv2.threshhold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+        _, threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)
         start_frame = frame_bw
 
         if threshold.sum() > 300:
             alarm = True
+            threading.Thread(target=beep).start()
+            alarm = False
 
         cv2.imshow("camera", threshold)
 
     else:
         cv2.imshow("Camera", frame)
 
-    if alarm and alarm_mode:
-        threading.Thread(target = beep).start()
-        alarm = False
-
-    key_pressed = cv2.waitKey(30)
-    if key_pressed == ord('t'):
+    if cv2.waitKey(30) == ord('a'):
+        cv2.destroyWindow(1)
         alarm_mode = not alarm_mode
 
-    if key_pressed == ord('e'):
+    if cv2.waitKey(30) == ord('e'):
         alarm_mode = False
         break
 
