64a8ad396e67db6a1e93309f0c17531ed6e390dd
Mihan <87608466+Computer4062@users.noreply.github.com>
2023-05-16 17:38:22 +0400

    Update run.py

--- Letter identifier/run.py
+++ Letter identifier/run.py
@@ -1,12 +1,18 @@
-import tensorflow as tf
-import cv2
+import matplotlib.pyplot as plt
+from keras.datasets import emnist
 import numpy as np
 
-model = tf.keras.models.load_model("handwrittern_letter_recognizer.model")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-img = cv2.imread("image.png")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_array = np.invert(np.array([img]))
-prediction = model.predict(img_array)
+(x_train, y_train), (x_test, y_test) = emnist.load_data()
 
-print(f">{np.argmax(prediction)}")
+image_index = 42
+selected_image = x_train[image_index]
+
+plt.imshow(selected_image, cmap='gray')
+plt.show()
+
+prediction = model.predict(selected_image)
+predicted = np.argmax(prediction)
+
+print(f">{alphabet[np.argmax(prediction)]}")
